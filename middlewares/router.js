2fea6240a229587af98c9df49e3958ce91b8ed86
priit <priit@bitweb.ee>
2015-08-19 11:53:07 +0300

    Sünkroonset teenust käivitatakse vaid korra

--- middlewares/router.js
+++ middlewares/router.js
@@ -6,7 +6,7 @@ var logger = require('log4js').getLogger('router_middleware');
 
 module.exports = function(req, res, next){
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-access-token");
 
     logger.info('Something is happening in: ' + req.url);
     next();
