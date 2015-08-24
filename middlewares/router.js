ad74e115fd6403881303795823d21b4810809b7d
priit <priit@bitweb.ee>
2015-08-24 16:40:58 +0300

    Api täiendused

--- middlewares/router.js
+++ middlewares/router.js
@@ -7,6 +7,7 @@ var logger = require('log4js').getLogger('router_middleware');
 module.exports = function(req, res, next){
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, X-Auth-UserId, X-Auth-Token, x-access-token");
+    res.header("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT");
 
     logger.info('Something is happening in: ' + req.url);
     next();
