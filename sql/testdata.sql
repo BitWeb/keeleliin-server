de6b86110833c7c9ddb780a134268eed3b0ba379
priit <priit@bitweb.ee>
2015-09-15 16:56:13 +0300

    KEEL-98

--- sql/testdata.sql
+++ sql/testdata.sql
@@ -1,18 +1,28 @@
+INSERT INTO "user" (entu_id, email, name, created_at, updated_at) SELECT 3021, 'priit@bitweb.ee', 'priit', now(), now() WHERE NOT EXISTS (SELECT 1 FROM "user" WHERE "user".email = 'priit@bitweb.ee');
+
 INSERT INTO resource_type (value, name, split_type) SELECT 'text', 'Tekst', 'LINE' WHERE NOT EXISTS (SELECT 1 FROM resource_type WHERE resource_type.id = 1);
 INSERT INTO resource_type (value, name, split_type) SELECT 'zip', 'Zip-file', 'NONE' WHERE NOT EXISTS (SELECT 1 FROM resource_type WHERE resource_type.id = 2);
 
-INSERT INTO "user" (entu_id, email, name, created_at, updated_at) SELECT 1, 'taivo@bitweb.ee', 'taivo', now(), now() WHERE NOT EXISTS (SELECT 1 FROM "user" WHERE "user".email = 'taivo@bitweb.ee');
-INSERT INTO "user" (entu_id, email, name, created_at, updated_at) SELECT 2, 'priit@bitweb.ee', 'priit', now(), now() WHERE NOT EXISTS (SELECT 1 FROM "user" WHERE "user".email = 'priit@bitweb.ee');
-
-INSERT INTO service (sid, name, url, created_at, updated_at) SELECT 'lau', 'Lausestaja', 'http://dev.bitweb.ee:3001/api/v1/', now(), now() WHERE NOT EXISTS (SELECT 1 FROM service WHERE service.sid = 'lau');
-INSERT INTO service (sid, name, url, created_at, updated_at) SELECT 'moa', 'Morfoloogiline analüüs', 'http://dev.bitweb.ee:3002/api/v1/', now(), now() WHERE NOT EXISTS (SELECT 1 FROM service WHERE service.sid = 'moa');
-INSERT INTO service (sid, name, url, created_at, updated_at) SELECT 'osl', 'Osalausestaja', 'http://dev.bitweb.ee:3003/api/v1/', now(), now() WHERE NOT EXISTS (SELECT 1 FROM service WHERE service.sid = 'osl');
-INSERT INTO service (sid, name, url, created_at, updated_at) SELECT 'moy', 'Morfoloogiline ühestamine (kitsenduste grammatika)', 'http://dev.bitweb.ee:3004/api/v1/', now(), now() WHERE NOT EXISTS (SELECT 1 FROM service WHERE service.sid = 'moy');
-INSERT INTO service (sid, name, url, created_at, updated_at) SELECT 'pia', 'Pindsüntaktiline analüüs', 'http://dev.bitweb.ee:3005/api/v1/', now(), now() WHERE NOT EXISTS (SELECT 1 FROM service WHERE service.sid = 'pia');
-INSERT INTO service (sid, name, url, created_at, updated_at) SELECT 's6a', 'Sõltuvussüntaktiline analüüs (ja järeltöötlus)', 'http://dev.bitweb.ee:3006/api/v1/', now(), now() WHERE NOT EXISTS (SELECT 1 FROM service WHERE service.sid = 's6a');
-INSERT INTO service (sid, name, url, created_at, updated_at) SELECT 'uzip', 'Arhiivi lahtipakkija', 'http://dev.bitweb.ee:3007/api/v1/', now(), now() WHERE NOT EXISTS (SELECT 1 FROM service WHERE service.sid = 'uzip');
-INSERT INTO service (sid, name, url, created_at, updated_at) SELECT 'tok', 'Sõnestaja pipe', 'http://dev.bitweb.ee:3008/api/v1/', now(), now() WHERE NOT EXISTS (SELECT 1 FROM service WHERE service.sid = 'tok');
-INSERT INTO service (sid, name, url, created_at, updated_at, is_synchronous) SELECT 'loco-test', 'LOCO test', 'http://localhost:3001/api/v1/', now(), now(), TRUE WHERE NOT EXISTS (SELECT 1 FROM service WHERE service.sid = 'loco-test');
+INSERT INTO service (sid, name, url, created_at, updated_at) SELECT 'lau', 'Lausestaja', 'http://dev.bitweb.ee:3001/api/v1/', now(), now()
+    WHERE NOT EXISTS (SELECT 1 FROM service WHERE service.id = 1);
+INSERT INTO service (sid, name, url, created_at, updated_at) SELECT 'moa', 'Morfoloogiline analüüs', 'http://dev.bitweb.ee:3002/api/v1/', now(), now()
+    WHERE NOT EXISTS (SELECT 1 FROM service WHERE service.id = 2);
+INSERT INTO service (sid, name, url, created_at, updated_at) SELECT 'osl', 'Osalausestaja', 'http://dev.bitweb.ee:3003/api/v1/', now(), now()
+    WHERE NOT EXISTS (SELECT 1 FROM service WHERE service.id = 3);
+INSERT INTO service (sid, name, url, created_at, updated_at) SELECT 'moy', 'Morfoloogiline ühestamine (kitsenduste grammatika)', 'http://dev.bitweb.ee:3004/api/v1/', now(), now()
+    WHERE NOT EXISTS (SELECT 1 FROM service WHERE service.id = 4);
+INSERT INTO service (sid, name, url, created_at, updated_at) SELECT 'pia', 'Pindsüntaktiline analüüs', 'http://dev.bitweb.ee:3005/api/v1/', now(), now()
+    WHERE NOT EXISTS (SELECT 1 FROM service WHERE service.id = 5);
+INSERT INTO service (sid, name, url, created_at, updated_at) SELECT 's6a', 'Sõltuvussüntaktiline analüüs (ja järeltöötlus)', 'http://dev.bitweb.ee:3006/api/v1/', now(), now()
+    WHERE NOT EXISTS (SELECT 1 FROM service WHERE service.id = 6);
+INSERT INTO service (sid, name, url, created_at, updated_at) SELECT 'uzip', 'Arhiivi lahtipakkija', 'http://dev.bitweb.ee:3007/api/v1/', now(), now()
+    WHERE NOT EXISTS (SELECT 1 FROM service WHERE service.id = 7);
+INSERT INTO service (sid, name, url, created_at, updated_at) SELECT 'tok', 'Sõnestaja pipe', 'http://dev.bitweb.ee:3008/api/v1/', now(), now()
+    WHERE NOT EXISTS (SELECT 1 FROM service WHERE service.id = 8);
+INSERT INTO service (sid, name, url, created_at, updated_at, is_synchronous) SELECT 'concat', 'Sünkroonne teksti konkateneerija', 'http://localhost:3009/api/v1/', now(), now(), TRUE
+    WHERE NOT EXISTS (SELECT 1 FROM service WHERE service.id = 9);
+INSERT INTO service (sid, name, url, created_at, updated_at, is_synchronous) SELECT 'moyp', 'Morfoloogiline ühestaja pipe', 'http://localhost:3010/api/v1/', now(), now(), TRUE
+    WHERE NOT EXISTS (SELECT 1 FROM service WHERE service.id = 10);
 
 INSERT INTO service_param (service_id, key, value) SELECT 1, 'isAsync', '1' WHERE NOT EXISTS (SELECT 1 FROM service_param WHERE service_param.id = 1);
 INSERT INTO service_param (service_id, key, value) SELECT 2, 'isAsync', '1' WHERE NOT EXISTS (SELECT 1 FROM service_param WHERE service_param.id = 2);
@@ -23,6 +33,7 @@ INSERT INTO service_param (service_id, key, value) SELECT 6, 'isAsync', '1' WHER
 INSERT INTO service_param (service_id, key, value) SELECT 7, 'isAsync', '1' WHERE NOT EXISTS (SELECT 1 FROM service_param WHERE service_param.id = 7);
 INSERT INTO service_param (service_id, key, value) SELECT 8, 'isAsync', '1' WHERE NOT EXISTS (SELECT 1 FROM service_param WHERE service_param.id = 8);
 INSERT INTO service_param (service_id, key, value) SELECT 9, 'isAsync', '1' WHERE NOT EXISTS (SELECT 1 FROM service_param WHERE service_param.id = 9);
+INSERT INTO service_param (service_id, key, value) SELECT 10, 'isAsync', '1' WHERE NOT EXISTS (SELECT 1 FROM service_param WHERE service_param.id = 10);
 
 INSERT INTO service_input_type(key, do_parallel, service_id, resource_type_id, size_limit, size_unit) SELECT 'content', FALSE, 1, 1, 0, 'byte' WHERE NOT EXISTS (SELECT 1 FROM service_input_type WHERE service_input_type.id = 1);
 INSERT INTO service_input_type(key, do_parallel, service_id, resource_type_id, size_limit, size_unit) SELECT 'content', FALSE, 2, 1, 0, 'byte' WHERE NOT EXISTS (SELECT 1 FROM service_input_type WHERE service_input_type.id = 2);
@@ -30,9 +41,10 @@ INSERT INTO service_input_type(key, do_parallel, service_id, resource_type_id, s
 INSERT INTO service_input_type(key, do_parallel, service_id, resource_type_id, size_limit, size_unit) SELECT 'content', FALSE, 4, 1, 0, 'byte' WHERE NOT EXISTS (SELECT 1 FROM service_input_type WHERE service_input_type.id = 4);
 INSERT INTO service_input_type(key, do_parallel, service_id, resource_type_id, size_limit, size_unit) SELECT 'content', FALSE, 5, 1, 0, 'byte' WHERE NOT EXISTS (SELECT 1 FROM service_input_type WHERE service_input_type.id = 5);
 INSERT INTO service_input_type(key, do_parallel, service_id, resource_type_id, size_limit, size_unit) SELECT 'content', FALSE, 6, 1, 0, 'byte' WHERE NOT EXISTS (SELECT 1 FROM service_input_type WHERE service_input_type.id = 6);
-INSERT INTO service_input_type(key, do_parallel, service_id, resource_type_id, size_limit, size_unit) SELECT 'content', FALSE, 7, 1, 0, 'byte' WHERE NOT EXISTS (SELECT 1 FROM service_input_type WHERE service_input_type.id = 7);
+INSERT INTO service_input_type(key, do_parallel, service_id, resource_type_id, size_limit, size_unit) SELECT 'content', FALSE, 7, 2, 0, 'byte' WHERE NOT EXISTS (SELECT 1 FROM service_input_type WHERE service_input_type.id = 7);
 INSERT INTO service_input_type(key, do_parallel, service_id, resource_type_id, size_limit, size_unit) SELECT 'content', FALSE, 8, 1, 0, 'byte' WHERE NOT EXISTS (SELECT 1 FROM service_input_type WHERE service_input_type.id = 8);
 INSERT INTO service_input_type(key, do_parallel, service_id, resource_type_id, size_limit, size_unit, is_list) SELECT 'content', FALSE, 9, 1, 0, 'byte', TRUE WHERE NOT EXISTS (SELECT 1 FROM service_input_type WHERE service_input_type.id = 9);
+INSERT INTO service_input_type(key, do_parallel, service_id, resource_type_id, size_limit, size_unit) SELECT 'content', FALSE, 10, 1, 0, 'byte' WHERE NOT EXISTS (SELECT 1 FROM service_input_type WHERE service_input_type.id = 10);
 
 INSERT INTO service_output_type(key, service_id, resource_type_id) SELECT 'output', 1, 1 WHERE NOT EXISTS (SELECT 1 FROM service_output_type WHERE service_output_type.id = 1);
 INSERT INTO service_output_type(key, service_id, resource_type_id) SELECT 'output', 2, 1 WHERE NOT EXISTS (SELECT 1 FROM service_output_type WHERE service_output_type.id = 2);
@@ -42,38 +54,20 @@ INSERT INTO service_output_type(key, service_id, resource_type_id) SELECT 'outpu
 INSERT INTO service_output_type(key, service_id, resource_type_id) SELECT 'output', 6, 1 WHERE NOT EXISTS (SELECT 1 FROM service_output_type WHERE service_output_type.id = 6);
 INSERT INTO service_output_type(key, service_id, resource_type_id) SELECT 'output', 7, 1 WHERE NOT EXISTS (SELECT 1 FROM service_output_type WHERE service_output_type.id = 7);
 INSERT INTO service_output_type(key, service_id, resource_type_id) SELECT 'output', 8, 1 WHERE NOT EXISTS (SELECT 1 FROM service_output_type WHERE service_output_type.id = 8);
+INSERT INTO service_output_type(key, service_id, resource_type_id) SELECT 'output', 9, 1 WHERE NOT EXISTS (SELECT 1 FROM service_output_type WHERE service_output_type.id = 9);
+INSERT INTO service_output_type(key, service_id, resource_type_id) SELECT 'output', 10, 1 WHERE NOT EXISTS (SELECT 1 FROM service_output_type WHERE service_output_type.id = 10);
 
-INSERT INTO project (name, description, user_id, created_at, updated_at) SELECT 'test project', 'test project', 1, now(), now() WHERE NOT EXISTS (SELECT 1 FROM project WHERE project.name = 'test project');
-INSERT INTO workflow_definition (project_id, user_id, name, description, created_at, updated_at) SELECT 1, 1, 'Test workflow', 'Test workflow', now() , now() WHERE NOT EXISTS (SELECT 1 FROM workflow_definition WHERE workflow_definition.id = 1);
-INSERT INTO project_workflow_definition (project_id, workflow_definition_id, created_at, updated_at) SELECT 1, 1, now(), now() WHERE NOT EXISTS (SELECT 1 FROM project_workflow_definition WHERE project_workflow_definition.project_id = 1 AND project_workflow_definition.workflow_definition_id = 1);
-
-INSERT INTO workflow_definition_service (workflow_definition_id, service_id, order_num, created_at, updated_at)
-SELECT 1, 1, 0, NOW(), NOW() WHERE NOT EXISTS (SELECT 1 FROM workflow_definition_service WHERE workflow_definition_service.id = 1);
-INSERT INTO workflow_definition_service (workflow_definition_id, service_id, order_num, created_at, updated_at)
-SELECT 1, 2, 1, NOW(), NOW() WHERE NOT EXISTS (SELECT 1 FROM workflow_definition_service WHERE workflow_definition_service.id = 2);
-INSERT INTO workflow_definition_service (workflow_definition_id, service_id, order_num, created_at, updated_at)
-SELECT 1, 3, 2, NOW(), NOW() WHERE NOT EXISTS (SELECT 1 FROM workflow_definition_service WHERE workflow_definition_service.id = 3);
-INSERT INTO workflow_definition_service (workflow_definition_id, service_id, order_num, created_at, updated_at)
-SELECT 1, 4, 3, NOW(), NOW() WHERE NOT EXISTS (SELECT 1 FROM workflow_definition_service WHERE workflow_definition_service.id = 4);
-INSERT INTO workflow_definition_service (workflow_definition_id, service_id, order_num, created_at, updated_at)
-SELECT 1, 5, 4, NOW(), NOW() WHERE NOT EXISTS (SELECT 1 FROM workflow_definition_service WHERE workflow_definition_service.id = 5);
-INSERT INTO workflow_definition_service (workflow_definition_id, service_id, order_num, created_at, updated_at)
-SELECT 1, 6, 5, NOW(), NOW() WHERE NOT EXISTS (SELECT 1 FROM workflow_definition_service WHERE workflow_definition_service.id = 6);
-
-INSERT INTO workflow_definition_service_param_value (workflow_definition_service_id, service_param_id) SELECT 1, 1 WHERE NOT EXISTS (
-SELECT 1 FROM workflow_definition_service_param_value WHERE workflow_definition_service_param_value.id = 1);
-INSERT INTO workflow_definition_service_param_value (workflow_definition_service_id, service_param_id) SELECT 2, 2 WHERE NOT EXISTS (
-SELECT 1 FROM workflow_definition_service_param_value WHERE workflow_definition_service_param_value.id = 2);
-INSERT INTO workflow_definition_service_param_value (workflow_definition_service_id, service_param_id) SELECT 3, 3 WHERE NOT EXISTS (
-SELECT 1 FROM workflow_definition_service_param_value WHERE workflow_definition_service_param_value.id = 3);
-INSERT INTO workflow_definition_service_param_value (workflow_definition_service_id, service_param_id) SELECT 4, 4 WHERE NOT EXISTS (
-SELECT 1 FROM workflow_definition_service_param_value WHERE workflow_definition_service_param_value.id = 4);
-INSERT INTO workflow_definition_service_param_value (workflow_definition_service_id, service_param_id) SELECT 5, 5 WHERE NOT EXISTS (
-SELECT 1 FROM workflow_definition_service_param_value WHERE workflow_definition_service_param_value.id = 5);
-INSERT INTO workflow_definition_service_param_value (workflow_definition_service_id, service_param_id) SELECT 6, 6 WHERE NOT EXISTS (
-SELECT 1 FROM workflow_definition_service_param_value WHERE workflow_definition_service_param_value.id = 6);
-
-INSERT INTO "user" (entu_id, email, name, created_at, updated_at) SELECT 3, 'taivo.teder@gmail.com', 'taivo1', now(), now() WHERE NOT EXISTS (SELECT 1 FROM "user" WHERE "user".email = 'taivo.teder@gmail.com');
+INSERT INTO service_has_parent_service(service_sibling_id, service_parent_id) SELECT 2, 1 WHERE NOT EXISTS (SELECT 1 FROM service_has_parent_service WHERE service_sibling_id = 2 AND  service_parent_id = 1);
+INSERT INTO service_has_parent_service(service_sibling_id, service_parent_id) SELECT 3, 2 WHERE NOT EXISTS (SELECT 1 FROM service_has_parent_service WHERE service_sibling_id = 3 AND  service_parent_id = 2);
+INSERT INTO service_has_parent_service(service_sibling_id, service_parent_id) SELECT 4, 3 WHERE NOT EXISTS (SELECT 1 FROM service_has_parent_service WHERE service_sibling_id = 4 AND  service_parent_id = 3);
+INSERT INTO service_has_parent_service(service_sibling_id, service_parent_id) SELECT 5, 4 WHERE NOT EXISTS (SELECT 1 FROM service_has_parent_service WHERE service_sibling_id = 5 AND  service_parent_id = 4);
+INSERT INTO service_has_parent_service(service_sibling_id, service_parent_id) SELECT 6, 5 WHERE NOT EXISTS (SELECT 1 FROM service_has_parent_service WHERE service_sibling_id = 6 AND  service_parent_id = 5);
+INSERT INTO service_has_parent_service(service_sibling_id, service_parent_id) SELECT 1, 7 WHERE NOT EXISTS (SELECT 1 FROM service_has_parent_service WHERE service_sibling_id = 1 AND  service_parent_id = 7);
+INSERT INTO service_has_parent_service(service_sibling_id, service_parent_id) SELECT 8, 7 WHERE NOT EXISTS (SELECT 1 FROM service_has_parent_service WHERE service_sibling_id = 8 AND  service_parent_id = 7);
+INSERT INTO service_has_parent_service(service_sibling_id, service_parent_id) SELECT 9, 7 WHERE NOT EXISTS (SELECT 1 FROM service_has_parent_service WHERE service_sibling_id = 9 AND  service_parent_id = 7);
+INSERT INTO service_has_parent_service(service_sibling_id, service_parent_id) SELECT 10, 8 WHERE NOT EXISTS (SELECT 1 FROM service_has_parent_service WHERE service_sibling_id = 10 AND  service_parent_id = 8);
+INSERT INTO service_has_parent_service(service_sibling_id, service_parent_id) SELECT 8, 9 WHERE NOT EXISTS (SELECT 1 FROM service_has_parent_service WHERE service_sibling_id = 8 AND  service_parent_id = 9);
+INSERT INTO service_has_parent_service(service_sibling_id, service_parent_id) SELECT 1, 9 WHERE NOT EXISTS (SELECT 1 FROM service_has_parent_service WHERE service_sibling_id = 1 AND  service_parent_id = 9);
 
 INSERT INTO notification_type (url_template, application_context, message, is_send_email, code, notify_period_days, mail_subject, mail_template)
 SELECT 'http://someurl/id/{id}', 'workflow', 'Töövoog lõppes', TRUE, 'workflow-finished', 0, 'Töövoog lõppes', '<p>Vaata töövoogu <a href="{url}">siit</a>.</p>'
@@ -91,7 +85,4 @@ WHERE NOT EXISTS (SELECT 1 FROM notification_type WHERE notification_type.code =
 INSERT INTO notification_type (url_template, application_context, message, is_send_email, code, notify_period_days, mail_subject, mail_template)
 SELECT 'http://someurl/id/{id}', 'workflow', 'Töövoog jookseb endiselt', TRUE, 'workflow-still-running', 7, 'Töövoog jookseb endiselt', '<p>Vaata töövoogu <a href="{url}">siit</a>.</p>'
 WHERE NOT EXISTS (SELECT 1 FROM notification_type WHERE notification_type.code = 'workflow-still-running');
-
-INSERT INTO project_user (user_id, project_id, role)
-SELECT 1, 1, 'owner' WHERE NOT EXISTS (SELECT 1 FROM project_user WHERE project_user.user_id = 1 AND project_user.project_id = 1);
 
