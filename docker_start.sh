472afb59e9f3aac85f10fc9407b25b9fae835579
priit <priit@bitweb.ee>
2015-10-21 15:33:18 +0300

    Failide üleslaadimine

--- docker_start.sh
+++ docker_start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cp -R -u -p /src/config_dist.js /config/config.js
-ln -s /src/config.js /config/config.js
+ln -s /config/config.js /src/config.js
 forever start /src/app.js
 forever list
